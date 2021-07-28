c3ef699f08167e1af0606392d4226afc4b8f49bd
kingbaddo <wizzyblaq94@gmail.com>
2021-07-29 00:56:11 +0100

    context Reducer

--- src/Context/contextReducer.js
+++ src/Context/contextReducer.js
@@ -1,24 +1,17 @@
 
-const transactions = [
-    {
-        id: 1,
-       
 
-    },
-    {
-        id: 2,
-       
-    }
-]
 
 const contextReducer = (state, action) => {
+    let transactions;
+    
    switch (action.type) {
        case "DELETE_TRANSACTION":
-            transactions = state.filter((t) => t.id !== action.payload);
-            break;
+           transactions = state.filter((t) => t.id !== action.payload);
+            
+          return transactions;
 
            case "ADD_TRANSACTION":
-               transactions = [action.payload, ...state];
+              transactions = [action.payload, ...state];
                
                return transactions;
    
