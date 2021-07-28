acda27fd1b30d46a0fbbadb71b07bb514392d8dd
kingbaddo <wizzyblaq94@gmail.com>
2021-07-29 00:55:38 +0100

    mui styling

--- src/components/styles.js
+++ src/components/styles.js
@@ -1,12 +1,18 @@
 import { makeStyles } from '@material-ui/core';
 
-export default makeStyles(() => ({
+export default makeStyles((theme) => ({
     income: {
         borderBottom: '10px solid rgba(0, 255, 0, 0.5)',
     },
      expense: {
         borderBottom: '10px solid rgba(255, 0, 0, 0.5)',
-    }
+    },
+    appheader: {
+        zIndex: "2",
+        backgroundColor: "green",
+        colorPrimary: "hsl(247, 69%, 61%)",
+    },
+   
 
     
 }));
