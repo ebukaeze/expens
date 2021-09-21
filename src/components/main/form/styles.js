c2b2bec91e298f44eaa345d292d70ced7b702ce7
kingbaddo <wizzyblaq94@gmail.com>
2021-09-21 17:31:40 +0100

    mui styling

--- src/components/main/form/styles.js
+++ src/components/main/form/styles.js
@@ -11,17 +11,23 @@ export default makeStyles(() => ({
         marginTop: '20px',
         background: 'hsl(247, 69%, 61%);',
         color: '#fff',
-        width: '100%'
+        width: '100%',
+        '&:hover':{
+           background: 'hsl(247, 60%, 20%)',
+        },
     },
+    
     form: {
       width: '100%',
       padding: '1px 5px'
     },
     date: {
-        paddingTop: '20px',
-        marginLeft: '20px',
-        marginTop: '-20px',
-        width: '170px'
+        padding: "1px 5px",
+        width: '100%'
+    },
+    amount: {
+       padding: "1px 5px",
+        width: '100%'
     },
     
 }))
