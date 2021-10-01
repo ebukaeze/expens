a9b285a5406ad20c2ee4e227529b9cf80260d7b6
kingbaddo <wizzyblaq94@gmail.com>
2021-10-01 23:03:02 +0100

    form data for category

--- src/constants/categories.js
+++ src/constants/categories.js
@@ -7,29 +7,32 @@ export const incomeCategories = [
     { type: 'Business', amount: 0, color: incomeColors[0] },
     { type: 'Investment', amount: 0, color: incomeColors[1] },
     { type: 'Salary', amount: 0, color: incomeColors[2] },
-    { type: 'Labour', amount: 0, color: incomeColors[3] },
-    { type: 'Services', amount: 0, color: incomeColors[4] },
-    { type: '', amount: 0, color: incomeColors[5] },
-    { type: 'Business', amount: 0, color: incomeColors[6] },
-    { type: 'Business', amount: 0, color: incomeColors[7] },
-    { type: 'Other', amount: 0, color: incomeColors[8] },
+    { type: 'Extra income', amount: 0, color: incomeColors[3] },
+    { type: 'Deposits', amount: 0, color: incomeColors[4] },
+    { type: 'Lottery', amount: 0, color: incomeColors[5] },
+    { type: 'Gifts', amount: 0, color: incomeColors[6] },
+    { type: 'Salary', amount: 0, color: incomeColors[7] },
+    { type: 'Savings', amount: 0, color: incomeColors[8] },
+    { type: 'Rental income', amount: 0, color: incomeColors[8] },
 
 ];
 
 export const expenseCategories = [
     { type: 'Food', amount: 0, color: expenseColors[0] },
     { type: 'Travel', amount: 0, color: expenseColors[1] },
-    { type: 'Utility Bill', amount: 0, color: expenseColors[2] },
-    { type: 'Rent', amount: 0, color: expenseColors[3] },
+    { type: 'House', amount: 0, color: expenseColors[2] },
+    { type: 'Clothes', amount: 0, color: expenseColors[3] },
     { type: 'Health', amount: 0, color: expenseColors[4] },
     { type: 'Education', amount: 0, color: expenseColors[5] },
-    { type: 'Family', amount: 0, color: expenseColors[6] },
-    { type: 'Miscelleanous', amount: 0, color: expenseColors[7] },
+    { type: 'Shopping', amount: 0, color: expenseColors[6] },
+    { type: 'Electronics', amount: 0, color: expenseColors[7] },
     { type: 'Other', amount: 0, color: expenseColors[8] },
 
 ]
 
 
-expenseCategories.forEach((expense, index) => {
-    
-})
+export const resetCategories = () => {
+    incomeCategories.forEach((c) => c.amount = 0);
+    expenseCategories.forEach((c) => c.amount = 0);
+
+};
