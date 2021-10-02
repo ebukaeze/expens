6b5d5c5d6923d32fba38d41df19707f3985e4622
kingbaddo <wizzyblaq94@gmail.com>
2021-10-02 09:14:27 +0100

    passed data from custome hook to chartjs doughnut

--- src/components/Details.js
+++ src/components/Details.js
@@ -4,15 +4,19 @@ import {Doughnut } from 'react-chartjs-2';
 
 import useStyles from './styles'; 
 
+import useTransactions from '../useTransactions';
+
 function Details({title}) {
     const classes = useStyles();
+
+    const { total, chartData } = useTransactions(title);
 
     return (
         <Card  className={title === "Income" ? classes.income : classes.expense}>
            <CardHeader  title={title}/>
            <CardContent>
-               <Typography variant="h5">$500</Typography>
-               {/* <Doughnut data='DATA'/> */}
+               <Typography variant="h5">${total}</Typography>
+                <Doughnut data={chartData}/>
                </CardContent> 
         </Card>
     )
