961926b90943ebd3e9856a38387c1c3f5a38717f
kingbaddo <wizzyblaq94@gmail.com>
2021-10-02 09:11:24 +0100

    added custome hook to filter, get data for chartjs

---
+++ src/useTransactions.js
@@ -0,0 +1,39 @@
+import { useContext } from "react";
+
+import { ExpenseTrackerContext } from './Context/context';
+
+
+import { incomeCategories, expenseCategories, resetCategories } from "./constants/categories";
+
+const useTransactions = (title) => {
+ resetCategories();
+ const { transactions } = useContext(ExpenseTrackerContext);
+ const transactionPerType =  transactions.filter((t) => t.type === title);
+ const total = transactionPerType.reduce((acc, currVal) => acc += currVal.amount, 0);
+ const categories = title === 'Income' ? incomeCategories : expenseCategories;
+
+ console.log({ transactionPerType, total, categories });
+
+ transactionPerType.forEach((t) => {
+     const category = categories.find((c) => c.type === t.category)
+
+     if(category) category.amount += t.amount;
+ });
+
+ const filteredCategory = categories.filter((c) => c.amount > 0);
+
+ const chartData = {
+     datasets:[
+         {
+             data: filteredCategory.map((c) => c.amount),
+             backgroundColor: filteredCategory.map((c) => c.color),
+         }
+     ],
+     labels: filteredCategory.map((c) => c.type),
+ }
+
+   return { filteredCategory, total, chartData } 
+}
+
+
+export default useTransactions;
