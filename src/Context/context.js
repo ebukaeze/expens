a37400805512fc11eb80d7c29399a3ef3a85549d
kingbaddo <wizzyblaq94@gmail.com>
2021-10-22 13:24:54 +0100

    calculate total by adding and substracting

--- src/Context/context.js
+++ src/Context/context.js
@@ -2,7 +2,13 @@ import React, {useReducer, createContext } from 'react';
 import contextReducer  from './contextReducer';
 
 
-const initialState = [];
+const initialState = JSON.parse(localStorage.getItem('transactions')) || [{
+    "amount": 2000,
+    "category": "Salary",
+    "type": "Income",
+    "date": "2021-10-07",
+    "id": "f986210a-67aa-4097-844c-245894c636ae"
+}];
 
 export const ExpenseTrackerContext = createContext(initialState);
 
@@ -13,12 +19,15 @@ export const Provider = ({children}) => {
     const deleteTransaction = (id) => dispatch({type: "DELETE_TRANSACTION", payload: id});
 
     const addTransaction = (transaction) => dispatch({ type: "ADD_TRANSACTION", payload: transaction });
+
+    const balance = transactions.reduce((acc, currVal) => currVal.type === "Expense" ? acc - currVal.amount : acc + currVal.amount, 0);
     
     return (
      <ExpenseTrackerContext.Provider value={{
          deleteTransaction, 
          addTransaction,
          transactions,
+         balance
      }}>
       {children}
      </ExpenseTrackerContext.Provider>
