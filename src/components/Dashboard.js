da89e85b4f084618f32209a243759bcbaaf426de
kingbaddo <wizzyblaq94@gmail.com>
2021-10-22 13:16:51 +0100

    made changes to the dashboard

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -11,7 +11,7 @@ function Dashboard() {
     const classes = useStyle();
     return (
         <>
-           <AppBar position="static" className={classes.appheader} style={{background : "#6757e0"}}>
+          {/*  <AppBar position="static" className={classes.appheader} style={{background : "#6757e0"}}>
   <Toolbar>
    
     <Typography variant="h6" edge="center"className={classes.title}>
@@ -19,7 +19,7 @@ function Dashboard() {
     </Typography>
     
   </Toolbar>
-</AppBar>
+</AppBar> */}
         <section className="dashboard">
             <div className="dashboard__container grid">
                 <Link to="/expense" className="grid__card">
@@ -27,7 +27,7 @@ function Dashboard() {
                         <i className="uil track__icon"><Unicons.UilChartPie size="100"/></i>
                         <h2 className="card__tilte">Track Expenses</h2>
                         <div>
-                        <h3 className="card__subtitle">Follow up with the analysis</h3>
+                        <h3 className="card__subtitle">Follow up with your expenses</h3>
                          <span class="arrow__right"><Unicons.UilArrowRight /></span>
                         </div>
                         <div></div>
@@ -35,19 +35,21 @@ function Dashboard() {
                 </Link>
                  <Link to="/income" className="grid__card">
                     <div className="card__content">
-                        <i className="uil track__icon"><Unicons.UilAnalysis size="100"/></i>
+                        <i className="uil track__icon"><Unicons.UilAnalytics size="100"/></i>
                         <h2 className="card__tilte">Track Income</h2>
                         <div>
-                        <h3 className="card__subtitle">Follow up with the analysis</h3>
+                        <h3 className="card__subtitle">Follow up with your income</h3>
+                        <span class="arrow__right"><Unicons.UilArrowRight /></span>
                         </div>
                     </div>
                 </Link>
-                 <Link to="/income" className="grid__card">
+                 <Link to="/analytics" className="grid__card">
                     <div className="card__content">
                         <i className="uil track__icon"><Unicons.UilAnalysis size="100"/></i>
                         <h2 className="card__tilte">Analytics</h2>
                         <div>
                         <h3 className="card__subtitle">Follow up with the analysis</h3>
+                        <span class="arrow__right"><Unicons.UilArrowRight /></span>
                         </div>
                     </div>
                 </Link>
