27803bd0f86d64bfe6beacddb9974833aed9df9a
kingbaddo <wizzyblaq94@gmail.com>
2021-10-22 13:22:29 +0100

    main card view

--- src/components/main/Main.js
+++ src/components/main/Main.js
@@ -1,20 +1,24 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Grid, Card, CardHeader, CardContent, Typography, Divider } from '@material-ui/core';
+
+import { ExpenseTrackerContext } from '../../Context/context';
 import useStyles from './styles';
 import Form from './form/Form';
 import List from './list/List';
+import InfoCard from '../InfoCard';
 
 
 const Main = () => {
     const classes = useStyles();
+    const { balance } = useContext(ExpenseTrackerContext);
     return (
         <Card className={classes.root}>
             <CardHeader title="Expense Tracker" subheader="Powered by Speechly"/>
             <CardContent>
-                <Typography align="center" variant="h5" style={{fontWeight: "bold", lineHeight: "1.5em", marginTop: "-8px"}}>Total Balance $1000 </Typography>
+               
+                <Typography align="center" variant="h5" style={{fontWeight: "bold", lineHeight: "1.5em", marginTop: "-8px"}}>Total Balance ${balance} </Typography>
                 <Typography variant="subtitle1" style={{lineHeight: "1.5em", marginTop: "12px", fontWeight: "500"}}>
-                     {/* InfoCard*/}
-                   Try saying: Add Income for $1000 in Category Salary for Monday ...
+                     <InfoCard />
                 </Typography>
                  <Divider />
                 <Form />
@@ -25,6 +29,7 @@ const Main = () => {
                   <Grid item xs={12}>
                        <List /> 
                   </Grid>
+                  
               </Grid>
             </CardContent>
         </Card>
